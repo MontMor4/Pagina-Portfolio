8b0d191011d8d1d82000440e72cccb14c9e3221c
MontMor4 <victorperial@hotmail.com>
2023-02-24 01:22:09 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleTheme() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/profilePicLight.png")
+  } else {
+    img.setAttribute("src", "./assets/profilePicDark.png")
+  }
+}
